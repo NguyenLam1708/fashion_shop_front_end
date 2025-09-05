fa21cbac035491ea2fa0e63700ec91ad915d2383
NguyenLam1708 <lam170804@gmail.com>
2025-09-05 16:21:13 +0700

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
+import Footer from './components/footer.vue'
 </script>
 
 <template>
@@ -8,12 +9,26 @@ import Navbar from './components/Navbar.vue'
     <main class="main">
       <router-view />
     </main>
+    <Footer />
   </div>
 </template>
 
 <style>
+html, body, #app {
+  margin: 0;
+  padding: 0;
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
 .main {
+  flex: 1; /* nội dung chiếm hết khoảng trống, đẩy footer xuống đáy */
   padding: 2rem;
-  margin-top: 70px; /* đẩy nội dung xuống dưới header */
+  margin-top: 70px; /* giữ khoảng trống cho header */
+}
+
+footer {
+  margin-top: auto;
 }
 </style>
