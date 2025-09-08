1692c4a781dd686f95e2045d7e4b73a5d3540806
NguyenLam1708 <lam170804@gmail.com>
2025-09-08 21:34:33 +0700

    feat: add router

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,26 @@
-import Register from '../pages/Register.vue'
-import Login from '../pages/Login.vue'
+import Register from '../pages/auth/Register.vue'
+import Login from '../pages/auth/Login.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
+import VerifyOtp from '../pages/auth/VerifyOtp.vue'
+import ForgotPassword from '../pages/user/ForgotPassword.vue'
+import Profile from '../pages/user/Profile.vue'
+import ChangePassword from '../pages/user/ChangePassword.vue'
+import CollectionsPage from '../pages/CollectionsPage.vue';
+import ProductDetail from '../pages/product/ProductDetail.vue';
 
 const routes = [
-  { path: '/', component: Home },
-    { path: '/register', component: Register },
-  { path: '/login', component: Login },
+  { path: '/', name: 'Home', component: Home },
+  { path: '/register', name: 'Register', component: Register },
+  { path: '/login', name: 'Login', component: Login },
+  { path: '/verify-otp', name: 'VerifyOtp', component: VerifyOtp },
+  { path: '/forgot-password', name: 'ForgotPassword', component: ForgotPassword },
+  { path: '/profile', name: 'Profile', component: Profile },
+  { path: '/change-password', name: 'ChangePassword', component: ChangePassword },
+  { path: '/collections/:slug', name: 'CollectionsPage', component: CollectionsPage, props: true },
+  { path: '/products/:id', name: 'ProductDetail', component: ProductDetail, props: true },
 ]
+
 
 const router = createRouter({
   history: createWebHistory(),
