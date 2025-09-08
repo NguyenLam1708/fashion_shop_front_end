3c5885573cb822c94b6e660880dbd9b992e68fbf
NguyenLam1708 <lam170804@gmail.com>
2025-09-08 21:34:50 +0700

    feat: fetch user

---
+++ src/stores/user.js
@@ -0,0 +1,31 @@
+import { defineStore } from "pinia"
+import api from "../services/api"
+
+export const useUserStore = defineStore("user", {
+  state: () => ({
+    user: null,
+    loading: false
+  }),
+  actions: {
+    setUser(user) {
+      this.user = user
+      localStorage.setItem("user", JSON.stringify(user))
+    },
+    clearUser() {
+      this.user = null
+      localStorage.removeItem("user")
+      localStorage.removeItem("token")
+    },
+    async fetchUser() {
+      this.loading = true
+      try {
+        const res = await api.get("/users/my-info")
+        this.setUser(res.data)
+      } catch (err) {
+        this.clearUser()
+      } finally {
+        this.loading = false
+      }
+    }
+  }
+})
