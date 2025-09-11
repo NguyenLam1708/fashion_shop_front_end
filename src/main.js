d05507eb2d6e45f0ffb33a40540b07b8e98409f3
NguyenLam1708 <lam170804@gmail.com>
2025-09-11 14:22:35 +0700

    feat:add package

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from '../src/router/index'
 import { createPinia } from 'pinia'
 import '@fortawesome/fontawesome-free/css/all.css';
+import Toast from "vue-toastification"
+import "vue-toastification/dist/index.css"
+import "element-plus/dist/index.css"
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+app.use(Toast)
 app.mount('#app')
